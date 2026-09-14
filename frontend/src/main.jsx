feed86b45389effd9cc782a1ca72f201d7458f8b
Dhairya Bansal <dhairyabansal090@gmail.com>
2026-09-14 20:56:39 +0530

    fix: switch frontend entry to icon-free production build

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./AppSafe.jsx";
+import App from "./AppFinal.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
