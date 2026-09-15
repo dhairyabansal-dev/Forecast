98b4e6b152135aee00a710a5677dd8d37f002e33
Dhairya Bansal <dhairyabansal090@gmail.com>
2026-09-15 06:58:28 +0530

    feat(auth): activate secure frontend authentication shell

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./AppFinal.jsx";
+import SecureApp from "./SecureApp.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <SecureApp />
     </HashRouter>
   </React.StrictMode>
 );
